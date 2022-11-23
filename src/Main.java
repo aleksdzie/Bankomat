f2da3804f3c0631fabc7c00628b62ee1017beaa0
aleksdzie <aleksandra.dziedzic1@gmail.com>
2022-11-23 16:55:58 +0100

    Gotowe bankomat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int stanKonta=1000;
+        System.out.println("Wprowadź kwotę, jaką chcesz wypłacić");
+        int kwota=scanner.nextInt();
+        if(kwota>stanKonta){
+            System.out.println("Przekroczono limit konta!");
+        }
+        else{
+            System.out.println("Kwota wypłaty: "+kwota);
+            System.out.println("Aktualny stan konta: "+(stanKonta-=kwota));
+        }
+
     }
 }
